0d80ef81c59d61fecf6b2c60aa83d6f9695de06a
Chance Robinson <brighamyoung2@gmail.com>
2023-10-30 00:36:50 -0600

    fixed api endpoint

--- crimeMapExample.js
+++ crimeMapExample.js
@@ -14,7 +14,7 @@ function loadMarkerClusterScript() {
     var clusterGroup = L.markerClusterGroup();
 
     // Fetch your data from a URL
-    fetch("http://127.0.0.1:8000/data", {
+    fetch("https://lapd-api.onrender.com/data", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
